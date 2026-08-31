be4488777f59d40f2843ff04b683206e738615c5
rlaehddud <6070kjh475@naver.com>
2026-08-31 11:17:27 +0900

    .

---
+++ chap08/src/chap08/WraooerDemo.java
@@ -0,0 +1,19 @@
+package chap08;
+
+public class WraooerDemo {
+
+	public static void main(String[] args) {
+		Integer bi1 = new Integer(10);
+		
+		int i = bi1.intValue();
+		double d = bi1.doubleValue();
+		Integer bi2=20;
+		int i2=bi2+20;
+		String s1 = Double.toString(3.14);
+		Double pi=Double.parseDouble("3.14");
+		Integer bi3=Integer.valueOf("11",16);
+		System.out.println(bi3);
+
+	}
+
+}
