b1435133842097b57bb85a2daae6c6a6c8639c06
rlaehddud <6070kjh475@naver.com>
2026-09-07 10:26:53 +0900

    .

--- chap08/src/chap08/RandomDemo.java
+++ chap08/src/chap08/RandomDemo.java
@@ -1,9 +1,15 @@
 package chap08;
 
+import java.util.Random;
+
 public class RandomDemo {
 
 	public static void main(String[] args) {
-
+		Random r = new Random();
+		
+		for (int i = 0; i<5; i++) {
+			System.out.print(r.nextInt(100) +" ");
+		}
 
 	}
 
